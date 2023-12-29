6328ebda4c9d3826c0e509fe4fefd7c06349a057
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2023-12-29 07:19:27 +0600

    05

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,14 +1,40 @@
 const UserInput = () => {
+  const handelFormSubmit = (event) => {
+    event.preventDefault();
+    console.log("handelFormSubmit");
+  };
+
+  const handelReset = (event) => {
+    event.preventDefault();
+    console.log("handelReset");
+  };
+
+  const inputChangeHandeler = (input, value) => {
+    console.log(input, value);
+  };
+
   return (
-    <form className="form">
+    <form onSubmit={handelFormSubmit} className="form">
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input
+            onChange={(e) =>
+              inputChangeHandeler("current-savings", e.target.value)
+            }
+            type="number"
+            id="current-savings"
+          />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input
+            onChange={(e) =>
+              inputChangeHandeler("yearly-contribution", e.target.value)
+            }
+            type="number"
+            id="yearly-contribution"
+          />
         </p>
       </div>
       <div className="input-group">
@@ -16,15 +42,25 @@ const UserInput = () => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input
+            onChange={(e) =>
+              inputChangeHandeler("expected-return", e.target.value)
+            }
+            type="number"
+            id="expected-return"
+          />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input
+            onChange={(e) => inputChangeHandeler("duration", e.target.value)}
+            type="number"
+            id="duration"
+          />
         </p>
       </div>
       <p className="actions">
-        <button type="reset" className="buttonAlt">
+        <button onClick={handelReset} type="reset" className="buttonAlt">
           Reset
         </button>
         <button type="submit" className="button">
